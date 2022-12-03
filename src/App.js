d0087970bfe6e1fe32d067050e61124af6186a86
parvez131037 <parvezahmed.ansari45@gmail.com>
2022-12-03 11:43:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Setting up the initial states using
+// react hook 'useState'
+const [search, setSearch] = useState("");
+const [subscribers, setSubscribers] = useState([]);
+
+//Fetching subscriber data from the backend API only
+//using UseEffect method
+useEffect(() => {
+	axios.get(`/subscribers/`).then((res) => {
+	setSubscribers(res.data);
+  console.log(res.data);
+	});
+}, []);
+
+
+return (
+	<div className="App">
+	<h1>ALMABETTER - GET YOUTUBE SUBSCRIBERS</h1>
+	<input
+		type="text"
+		placeholder="Search by name..."
+		onChange={(e) => {
+		setSearch(e.target.value);
+		}}
+	/>
+	<table>
+		<thead>
+		<tr>
+			<td>Subscriber ID</td>
+			<td>Subscriber Name</td>
+			<td>Subscribed Channel</td>
+			<td>Subscribed at</td>
+		</tr>
+		</thead>
+		{/* Mapping all the subscribers */}
+		<tbody>
+		{/* Filtering to check for the searched subscriber */}
+		{subscribers
+			.filter((val) => {
+			return val.name.toLowerCase().includes(search.toLowerCase());
+			})
+			.map((val, id) => {
+			return (
+				<>
+				<tr id={id}>
+					<td className="subsId">{val._id}</td>
+					<td className="subsName">{val.name}</td>
+					<td className="subsChannel">{val.subscribedChannel}</td>
+					<td className="subsDate">{val.subscribedDate}</td>
+				</tr>
+				</>
+			);
+			})}
+		</tbody>
+	</table>
+	</div>
+);
 }
 
 export default App;
